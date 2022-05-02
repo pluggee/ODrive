d68cfe97000a68c7b46a99ad27673e89c108128e
Sherif Eid <sherif@plugg.ee>
2022-05-02 10:37:59 -0700

    added stopmotors

---
+++ automation/stopmotors.py
@@ -0,0 +1,18 @@
+""" Boot and configure odrive """
+
+import odrive
+from odrive.enums import *
+import time
+
+# init odrv0
+odrv0 = odrive.find_any()
+odrv0.clear_errors()
+
+
+print('---------------------------------------------------------------')
+print('| stopping all motors')
+
+odrv0.axis0.requested_state = AXIS_STATE_IDLE
+odrv0.axis1.requested_state = AXIS_STATE_IDLE
+
+
